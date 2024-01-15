a9966423f79ad16fdafec1208c02847c4eed0fe0
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-01-15 23:11:43 +0100

    fix: Wrong utilization of reinterpret_cast

--- src/SerializationUtils.hxx
+++ src/SerializationUtils.hxx
@@ -11,7 +11,7 @@ inline auto serialize(std::ostream &out, const Enum &e) -> std::ostream &
   // https://www.reddit.com/r/cpp_questions/comments/30u26m/how_can_i_restrict_e_in_template_typename_e_to_be/
   static_assert(std::is_enum<Enum>::value, "Expected to be used with enums!");
 
-  const auto eAsChar = reinterpret_cast<const char *>(e);
+  const auto eAsChar = reinterpret_cast<const char *>(&e);
   const auto size    = sizeof(typename std::underlying_type<Enum>::type);
   for (auto id = 0u; id < size; ++id)
   {
@@ -26,7 +26,7 @@ inline auto deserialize(std::istream &in, Enum &e) -> std::istream &
 {
   static_assert(std::is_enum<Enum>::value, "Expected to be used with enums!");
 
-  const auto eAsChar = reinterpret_cast<char *>(e);
+  const auto eAsChar = reinterpret_cast<char *>(&e);
   const auto size    = sizeof(typename std::underlying_type<Enum>::type);
   for (auto id = 0u; id < size; ++id)
   {
